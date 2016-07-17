c5d6ce9fed1b7f31779b100617b08050f497c702
curiosity26 <curiosity26@gmail.com>
2016-07-17 16:18:06 -0400

    Update deployment info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var url = require("url");
 var jsonld_request = require("jsonld-request");
-var port = process.argv[3] || 80;
+var port = process.argv[2] || 80;
 
 var server = http.createServer(function(req, res) {
 
